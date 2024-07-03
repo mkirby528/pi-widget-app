58968d34c33abc2533b38fec519910c638bfae33
Matthew Kirby <mkirby2899@gmail.com>
2024-07-02 21:00:34 -0400

    clean up code for google endpoints

--- api/constants.py
+++ api/constants.py
@@ -31,3 +31,5 @@ class API_ENDPOINTS():
         OPEN_WEATHER_PARAMETERS.LATTITUDE}&lon={OPEN_WEATHER_PARAMETERS.LONGITUDE}&appid={OPEN_WEATHER_PARAMETERS.API_KEY}&exclude={OPEN_WEATHER_PARAMETERS.EXCLUDE}&units={OPEN_WEATHER_PARAMETERS.UNITS}"
     WMATA = f"https://api.wmata.com/StationPrediction.svc/json/GetPrediction/C05"
     GOOGLE_REFRESH_TOKEN = f"https://www.googleapis.com/oauth2/v4/token"
+    GOOGLE_PHOTOS = "https://photoslibrary.googleapis.com/v1/mediaItems:search?access_token={access_token}"
+    GOOGLE_CALENDAR = "https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events"
--- api/services/google_photos.py
+++ api/services/google_photos.py
@@ -10,8 +10,6 @@ from utils import get_google_access_token
 def get_all_image_urls_for_album_id(album_id: str):
     photos = []
     access_token = get_google_access_token()
-    url = f"https://photoslibrary.googleapis.com/v1/mediaItems:search?access_token={
-        access_token}"
     request_body = {
         "albumId": album_id,
         "pageSize": 100
@@ -20,11 +18,12 @@ def get_all_image_urls_for_album_id(album_id: str):
     while True:
         if next_page_token:
             request_body["pageToken"] = next_page_token
-        response = requests.post(url, request_body)
+
+        response = requests.post(API_ENDPOINTS.GOOGLE_PHOTOS.format(access_token=access_token), request_body)
         logger.info(f"Google photo response status: {response.status_code}")
         urls = [f"{photo.get("baseUrl")}=w2048-h1024"
                 for photo in response.json().get("mediaItems")]
-        photos += urls
+        photos.extend += urls
         next_page_token = response.json().get("nextPageToken", "")
         if not next_page_token:
             break
