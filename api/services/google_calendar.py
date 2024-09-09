01eef5a2124fcdbce6af25c401fe24d6c533b104
Matthew Kirby <mkirby2899@gmail.com>
2024-09-09 00:53:56 -0400

    add timezones to all day events

--- api/services/google_calendar.py
+++ api/services/google_calendar.py
@@ -2,9 +2,10 @@ from utils import get_google_access_token
 from datetime import datetime
 import requests
 from constants import API_ENDPOINTS
+from dateutil.parser import parse
 
 
-def get_calendar_events(calendar_id="primary",num_events=25):
+def get_calendar_events(calendar_id="primary", num_events=25):
     access_token = get_google_access_token()
     minimum_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
@@ -13,13 +14,14 @@ def get_calendar_events(calendar_id="primary",num_events=25):
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
-    respose = requests.get(API_ENDPOINTS.GOOGLE_CALENDAR.format(calendar_id=calendar_id), headers=headers, params=querystring)
+    respose = requests.get(API_ENDPOINTS.GOOGLE_CALENDAR.format(
+        calendar_id=calendar_id), headers=headers, params=querystring)
     calendar_items = respose.json().get("items")
     calendar_items = [
         {
             "event_title": item.get("summary"),
-            "start_time": item.get("start", {}).get("date") or item.get("start", {}).get("dateTime"),
-            "end_time": item.get("end", {}).get("date") or item.get("end", {}).get("dateTime"),
+            "start_time": parse(item.get("start", {}).get("date") or item.get("start", {}).get("dateTime")),
+            "end_time": parse(item.get("end", {}).get("date") or item.get("end", {}).get("dateTime")),
             "color": item.get("colorId", "0")
         } for item in calendar_items[:num_events]]
     return calendar_items
