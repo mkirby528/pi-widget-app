b94664e0bf09364174f08645a3a66beabe472749
Matthew Kirby <mkirby2899@gmail.com>
2024-11-17 22:57:44 -0500

    add speach recogntion test

--- ui/src/Components/SpeachRecognitionPage.tsx
+++ ui/src/Components/SpeachRecognitionPage.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
+
+
 const Dictaphone = () => {
     const {
         transcript,
         listening,
-        resetTranscript,
         browserSupportsSpeechRecognition
     } = useSpeechRecognition();
+    const startListening = () => SpeechRecognition.startListening({ continuous: true });
 
     if (!browserSupportsSpeechRecognition) {
         return <span>Browser doesn't support speech recognition.</span>;
     }
 
-    if (!browserSupportsSpeechRecognition) {
-        return <div>"Not supported"</div>
-    }
-    else {
-        return (
-
-            <div>
-                <p>Microphone: {listening ? 'on' : 'off'}</p>
-                <button onClick={SpeechRecognition.startListening}>Start</button>
-                <button onClick={SpeechRecognition.stopListening}>Stop</button>
-                <button onClick={resetTranscript}>Reset</button>
-                <p>{transcript}</p>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <button
+                onTouchStart={startListening}
+                onMouseDown={startListening}
+            >Press to talk</button>
+             <button
+                onTouchStart={SpeechRecognition.stopListening}
+                onMouseDown={SpeechRecognition.stopListening}
+            >Stop</button>
+            <p>{transcript}</p>
+        </div>
+    );
 };
 export default Dictaphone;
